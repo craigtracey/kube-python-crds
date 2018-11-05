ddfb6886f4f4ac2da175cead2d31723ad2e8d99a
Ubuntu <ubuntu@ip-10-0-26-37.ec2.internal>
2018-11-05 14:06:16 +0000

    Add missing generic_obj file

---
+++ generic_obj.py
@@ -0,0 +1,77 @@
+# coding: utf-8
+
+from pprint import pformat
+from six import iteritems
+import re
+
+
+class GenericObject(object):
+    
+    def __init__(self, api_version=None, kind=None, metadata=None, spec=None, status=None):
+        self.api_version = None
+        self.kind = None
+        self.metadata = None
+        self.spec = None
+        self.status = None
+        self.discriminator = None
+
+        if api_version is not None:
+          self.api_version = api_version
+        if kind is not None:
+          self.kind = kind
+        if metadata is not None:
+          self.metadata = metadata
+        if spec is not None:
+          self.spec = spec
+        if status is not None:
+          self.status = status
+
+    def to_dict(self):
+        """
+        Returns the model properties as a dict
+        """
+        result = {}
+
+        for attr, _ in iteritems(self.swagger_types):
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """
+        Returns the string representation of the model
+        """
+        return pformat(self.to_dict())
+
+    def __repr__(self):
+        """
+        For `print` and `pprint`
+        """
+        return self.to_str()
+
+    def __eq__(self, other):
+        """
+        Returns true if both objects are equal
+        """
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """
+        Returns true if both objects are not equal
+        """
+        return not self == other
